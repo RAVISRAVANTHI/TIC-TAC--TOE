7ce541c314d2389028027e067bcbed31601e2dac
BODEMPUDI.RAVISRAVANTHI <bodempudi.munnisrinivas@gmail.com>
2025-03-16 17:00:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
  <script>
-
-  <!-- 5x5 Grid of Buttons -->
-            
-                for (let i = 0; i < 30; i++) {
-                    document.write('<button class="box"></button>');
-                }
-           
         let boxes = document.querySelectorAll('.box');
         let resetBtn = document.querySelector('#reset');
         let newGameBtn = document.querySelector('#new-btn');
